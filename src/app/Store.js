382c1cbd0ee2c22dcc853d9c830e9e3f13d74ff4
sajib hossen <sajibhossen560@gmail.com>
2025-11-01 00:53:35 +0600

    store configer

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsSlice from '../features/products/Products.js'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { ProductsApi } from "../services/ProdutsApi.js";
 const store = configureStore({
     reducer :{
+         [ProductsApi.reducerPath]: ProductsApi.reducer,
         productsR : productsSlice
-    }
+    },
+     middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(ProductsApi.middleware),
 })
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+// setupListeners(store.dispatch)
 
 export default store
