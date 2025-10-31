93f4595e41f7959bc6592f952dda98fc1e0e1b5a
sajib hossen <sajibhossen560@gmail.com>
2025-11-01 02:02:05 +0600

    delete fecther complated

--- database/Db.js
+++ database/Db.js
@@ -1,42 +1,6 @@
 {
   "products": [
     {
-      "id": "1",
-      "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      "price": 109.95,
-      "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday essentials, and more.",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      "rating": {
-        "rate": 4.9,
-        "count": 120
-      }
-    },
-    {
-      "id": "3",
-      "title": "Mens Cotton Jacket",
-      "price": 55.99,
-      "description": "Great outerwear jacket for Spring/Autumn/Winter, suitable for many occasions, durable and stylish.",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
-      "rating": {
-        "rate": 4.7,
-        "count": 500
-      }
-    },
-    {
-      "id": "4",
-      "title": "Mens Casual Slim Fit",
-      "price": 15.99,
-      "description": "The color could be slightly different between on the screen and in practice. Comfortable slim-fit design for daily wear.",
-      "category": "men's clothing",
-      "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-      "rating": {
-        "rate": 2.1,
-        "count": 430
-      }
-    },
-    {
       "id": "5",
       "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Bracelet",
       "price": 695,
--- src/services/ProdutsApi.js
+++ src/services/ProdutsApi.js
@@ -3,16 +3,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const ProductsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3003/' }),
+  tagTypes : ["product"],
   endpoints: (builder) => ({
     // query use onley red data
     getProducts: builder.query({
       query: () => 'products',
-    }),
+      providesTags: (result) =>
+  result
+    ? [...result.map(({ id }) => ({ type: 'Product', id })),
+        { type: 'Product', id: 'LIST' },
+      ]
+    : [{ type: 'Product', id: 'LIST' }],
+  }),
     deleteProduct : builder.mutation({
       query : (id) => ({
         url : `products/${id}`,
         method : 'DELETE'
-      })
+      }),
+      invalidatesTags : (results , error , id) => [{
+        type : 'Product' , id
+      }]
     })
   }),
 })
